550523b9e99f857eaec063d27e151276ad4af04e
bienculo8282 <139576246+bienculo8282@users.noreply.github.com>
2023-10-09 09:28:57 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
             <div className="col-md-6">
               <div className="site-branding-text">
                 <p className="site-title-footer">
-                  <a href="https://dailyxpresss.com/" rel="home">Daily Express</a>
+                  <a href="https://apship.vn/" rel="home">Daily Express</a>
                 </p>
                 <p className="site-description-footer">Read the best everyday</p>
               </div>
@@ -24,25 +24,25 @@ export default function Footer() {
             <div className="col-md-6 text-xs">
               <p>
                 <a href="https://wordpress.org/">Proudly powered by WordPress</a>
-                <span className="sep"> | </span> Theme: Newses by <a href="https://themeansar.com/" rel="designer">Themeansar</a>.
+                <span className="sep"> | </span> Theme: Newses by <a href="https://apship.vn/" rel="designer">Themeansar</a>.
               </p>
             </div>
             <div className="col-md-6 text-right footer-menu text-xs">
               <ul className="info-right">
                 <li className="nav-item menu-item active">
-                  <a className="nav-link " href="https://dailyxpresss.com/" title="Home">Home</a>
+                  <a className="nav-link " href="https://apship.vn/" title="Home">Home</a>
                 </li>
                 <li className="nav-item menu-item page_item dropdown page-item-49">
-                  <a className="nav-link" href="https://dailyxpresss.com/ccpa-california-consumer-privacy-act/">CCPA – California Consumer Privacy Act</a>
+                  <a className="nav-link" href="https://apship.vn/ccpa-california-consumer-privacy-act/">CCPA – California Consumer Privacy Act</a>
                 </li>
                 <li className="nav-item menu-item page_item dropdown page-item-46">
-                  <a className="nav-link" href="https://dailyxpresss.com/dmca/">DMCA</a>
+                  <a className="nav-link" href="https://apship.vn/dmca/">DMCA</a>
                 </li>
                 <li className="nav-item menu-item page_item dropdown page-item-39">
-                  <a className="nav-link" href="https://dailyxpresss.com/privacy-policy/">Privacy Policy</a>
+                  <a className="nav-link" href="https://apship.vn/privacy-policy/">Privacy Policy</a>
                 </li>
                 <li className="nav-item menu-item page_item dropdown page-item-43">
-                  <a className="nav-link" href="https://dailyxpresss.com/terms-of-use/">Terms of Use</a>
+                  <a className="nav-link" href="https://apship.vn/terms-of-use/">Terms of Use</a>
                 </li>
               </ul>
             </div>
